0c31880db7eca79663ceebb5b4d7324424facfe5
Yrjar V <yrjarvederhus@gmail.com>
2024-02-20 18:51:26 +0100

    Delete WebScraper.java

--- WebScraper.java
+++
@@ -1,9 +0,0 @@
-/**
- * The main program
- */
-public class WebScraper {
-    public static void main(String[] args) {
-        URLScanner first = new URLScanner(args[0]);
-        first.getAllLinks();
-    }
-}
